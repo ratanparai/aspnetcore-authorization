516c22c5118724fbda7c7280d0b029face503092
Ratan Sunder Parai <ratanparai@gmail.com>
2019-11-09 18:26:22 +0600

    add minimal in memory identity

---
+++ src/Services/Identity/Config.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Identity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            { };
+    }
+}
